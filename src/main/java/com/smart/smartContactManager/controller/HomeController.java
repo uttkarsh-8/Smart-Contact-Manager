87d1114dd3832cad078971f9dff63254c2298f08
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-01-27 19:59:36 +0530

    Added home page and sign up pages in templates

--- src/main/java/com/smart/smartContactManager/controller/HomeController.java
+++ src/main/java/com/smart/smartContactManager/controller/HomeController.java
@@ -4,21 +4,27 @@ import com.smart.smartContactManager.dao.UserRepository;
 import com.smart.smartContactManager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
 
-    @Autowired
-    private UserRepository userRepository;
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
-        User user = new User();
-        user.setName("uttu");
-        user.setEmail("uttu.sri@outlook.com");
-        userRepository.save(user);
-        return "Working";
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("title", "Register - Smart Contact Manager");
+        return "signup";
     }
 }
