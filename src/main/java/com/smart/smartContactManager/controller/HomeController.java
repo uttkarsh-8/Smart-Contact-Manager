175482cd988c375e01d0b80e124f6f2389ffdc9a
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-02-01 19:05:24 +0530

    implemented spring security, and handled errors gracefully

--- src/main/java/com/smart/smartContactManager/controller/HomeController.java
+++ src/main/java/com/smart/smartContactManager/controller/HomeController.java
@@ -2,16 +2,14 @@ package com.smart.smartContactManager.controller;
 
 import com.smart.smartContactManager.dao.UserRepository;
 import com.smart.smartContactManager.entities.User;
-import com.smart.smartContactManager.helper.Message;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
@@ -20,6 +18,7 @@ public class HomeController {
 
     @Autowired
     private UserRepository userRepository;
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Home - Smart Contact Manager");
@@ -42,23 +41,33 @@ public class HomeController {
     // handling for registering user
 
     @PostMapping("/do_register")
-    public String register(@Valid @ModelAttribute("user") User user , BindingResult result1  , Model model){
+    public String register(@Valid @ModelAttribute("user") User user, BindingResult result1, Model model) {
+        try {
+            if (result1.hasErrors()) {
+                System.out.println("ERROR" + result1);
+                model.addAttribute("user", user); // to keep the data in the form
+                return "signup";
+            }
 
-        if (result1.hasErrors()){
-            System.out.println("EEERRROOOOOOOORORRRRRRR HEEERRREEEEEEE"+result1);
-            model.addAttribute("user", user);
-            model.addAttribute("errors", result1);
+            System.out.println(user);
+            user.setEnabled(true);
+            user.setRole("ROLE_USER");
+            user.setImageUrl("default.png");
+            User result = userRepository.save(user);
+            model.addAttribute("user", new User()); // to clear the form
+            model.addAttribute("success", "User has been registered successfully"); // to show user is successfully registered
+                return "signup";
+
+
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            model.addAttribute("duplicateEmail", "Email already exists");
             return "signup";
-            } else {
-                    System.out.println(user);
-                    user.setEnabled(true);
-                    user.setRole("ROLE_USER");
-                    user.setImageUrl("default.png");
-                    User result = userRepository.save(user);
-                    model.addAttribute("user", new User());
-
-                    return "signup";
-                }
-
+        }catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("error", "An unexpected error occurred");
+            return "signup";
+        }
     }
 }
+
