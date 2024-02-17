bee5457529c6a505aa2a5c690f2e30cef7b018ab
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-02-18 03:58:10 +0530

    connected the contact list to the database

--- src/main/java/com/smart/smartContactManager/controller/UserController.java
+++ src/main/java/com/smart/smartContactManager/controller/UserController.java
@@ -1,43 +1,56 @@
-package com.smart.smartContactManager.controller;
+    package com.smart.smartContactManager.controller;
 
 
-import com.smart.smartContactManager.dao.UserRepository;
-import com.smart.smartContactManager.entities.Contact;
-import com.smart.smartContactManager.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+    import com.smart.smartContactManager.dao.UserRepository;
+    import com.smart.smartContactManager.entities.Contact;
+    import com.smart.smartContactManager.entities.User;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.ModelAttribute;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
+    import java.security.Principal;
 
-@Controller
-@RequestMapping("/user")
-public class UserController {
+    @Controller
+    @RequestMapping("/user")
+    public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    @ModelAttribute //method to add common data to response
-    public void addCommonData(Model model, Principal pricipal){
-        String userName = pricipal.getName();
-        User user = userRepository.getUserByUserName(userName);
-        model.addAttribute("user", user);
+        @ModelAttribute //method to add common data to response
+        public void addCommonData(Model model, Principal principal){
+            String userName = principal.getName();
+            User user = userRepository.getUserByUserName(userName);
+            model.addAttribute("user", user);
+        }
+
+        @GetMapping("/index")
+        public String dashboard(Model model){
+
+            return "normal/user_dashboard";
+        }
+
+        //add form handler
+        @GetMapping("/add-contact")
+        public String openAddContactForm(Model model){
+            model.addAttribute("title", "Add Contact");
+            model.addAttribute("contact", new Contact());
+            return "normal/add_contact_form";
+        }
+        // Processing add contact form
+        @PostMapping("/process-contact")
+        public String processContact(@ModelAttribute Contact contact, Principal principal){
+            System.out.println("Contact info is hererererer"+contact);
+            String name = principal.getName();
+            User user = userRepository.getUserByUserName(name);
+            contact.setUser(user);
+            user.getContacts().add(contact);
+            userRepository.save(user);
+            System.out.println("ADDED TO DATA BASE");
+            return "normal/add_contact_form";
+        }
     }
-
-    @GetMapping("/index")
-    public String dashboard(Model model){
-
-        return "normal/user_dashboard";
-    }
-
-    //add form handler
-    @GetMapping("/add-contact")
-    public String openAddContactForm(Model model){
-        model.addAttribute("title", "Add Contact");
-        model.addAttribute("contact", new Contact());
-        return "normal/add_contact_form";
-    }
-}
--- src/main/java/com/smart/smartContactManager/entities/User.java
+++ src/main/java/com/smart/smartContactManager/entities/User.java
@@ -1,14 +1,11 @@
 package com.smart.smartContactManager.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
